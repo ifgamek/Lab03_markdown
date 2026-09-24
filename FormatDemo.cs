580b2b5b48db85d95dfaa97f04fcc1383234e651
ifgamek <andkategik@gmail.com>
2026-09-24 08:54:32 +0300

    Completed README.md with full Markdown and LaTeX showcase

---
+++ FormatDemo.cs
@@ -0,0 +1,5 @@
+Console.WriteLine("введите перове число:");
+double n_1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("введите второе число:");
+double n_2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"**результат операции {n_1 + n_2}**");
